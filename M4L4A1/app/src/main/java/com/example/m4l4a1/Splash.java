fe8d08d0d32295e07a89cc46d670d04cfb55c2ae
Curriculum Team <93026261+CurriculumTeam@users.noreply.github.com>
2022-03-01 15:49:24 +0530

    Add files via upload

---
+++ M4L4A1/app/src/main/java/com/example/m4l4a1/Splash.java
@@ -0,0 +1,24 @@
+package com.example.m4l4a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
